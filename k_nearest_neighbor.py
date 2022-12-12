d66310250b833b019299cf769a8c29e0863b4efc
anishdhakal15 <anish.dhakal.ad.ad15@gmail.com>
2022-12-12 16:03:40 +0545

    k_nearest updated

--- k_nearest_neighbor.py
+++ k_nearest_neighbor.py
@@ -11,13 +11,13 @@ from sklearn import metrics
 path="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/teleCust1000t.csv"
 k = 4
 
-# # onetime 
-# def download(url, filename):
-#     response = requests.get(path)
-#     with open(filename, "wb") as f:
-#         f.write(response.content)
+# onetime 
+def download(url, filename):
+    response = requests.get(path)
+    with open(filename, "wb") as f:
+        f.write(response.content)
 
-# download(path, 'teleCust1000t.csv')
+download(path, 'teleCust1000t.csv')
 
 df = pd.read_csv('teleCust1000t.csv')
 X = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']] .values  #.astype(float)
@@ -32,3 +32,29 @@ neigh = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
 yhat = neigh.predict(X_test)
 print("Train set Accuracy: ", metrics.accuracy_score(y_train, neigh.predict(X_train)))
 print("Test set Accuracy: ", metrics.accuracy_score(y_test, yhat))
+
+# # calculate the accuracy of KNN for different values of k
+# Ks = 10
+# mean_acc = np.zeros((Ks-1))
+# std_acc = np.zeros((Ks-1))
+
+# for n in range(1,Ks):
+    
+#     #Train Model and Predict  
+#     neigh = KNeighborsClassifier(n_neighbors = n).fit(X_train,y_train)
+#     yhat=neigh.predict(X_test)
+#     mean_acc[n-1] = metrics.accuracy_score(y_test, yhat)
+
+    
+#     std_acc[n-1]=np.std(yhat==y_test)/np.sqrt(yhat.shape[0])
+
+
+# # Plot the model accuracy for a different number of neighbors
+# plt.plot(range(1,Ks),mean_acc,'g')
+# plt.fill_between(range(1,Ks),mean_acc - 1 * std_acc,mean_acc + 1 * std_acc, alpha=0.10)
+# plt.fill_between(range(1,Ks),mean_acc - 3 * std_acc,mean_acc + 3 * std_acc, alpha=0.10,color="green")
+# plt.legend(('Accuracy ', '+/- 1xstd','+/- 3xstd'))
+# plt.ylabel('Accuracy ')
+# plt.xlabel('Number of Neighbors (K)')
+# plt.tight_layout()
+# plt.show()
